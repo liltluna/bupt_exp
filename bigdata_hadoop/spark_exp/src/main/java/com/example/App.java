2303326581c49442a8f1e9a33b18145f1a8042a9
Pick-cmd <728820587@qq.com>
2022-11-26 00:51:06 +0800

    finish the homework

--- bigdata_hadoop/spark_exp/src/main/java/com/example/App.java
+++ bigdata_hadoop/spark_exp/src/main/java/com/example/App.java
@@ -1,27 +1,56 @@
 package com.example;
 
 import org.apache.spark.sql.SparkSession;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import org.apache.spark.sql.Dataset;
-import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.SparkConf;
 
 public class App {
   public static void main(String[] args) {
-    SparkConf conf = new SparkConf().setAppName("average calculate").setMaster("local");
-    JavaSparkContext sc = new JavaSparkContext(conf);
-
-    
     // Should be some file on your system
-    String logFile = "/usr/local/spark/README.md"; 
-    SparkSession spark = SparkSession.builder().appName("Average Calculate").getOrCreate();
-    Dataset<String> logData = spark.read().textFile(logFile).cache();
+    String logFiles[] = {"input/db.txt", "input/ds.txt", "input/os.txt"}; 
 
-    long numAs = logData.count();
-    // long numBs = logData.filter(s -> s.contains("b")).count();
+    // N records the number of course
+    int N = logFiles.length;
 
-    System.out.println("Lines with a: " + numAs + ", lines with b: " + numAs);
+    HashMap<String, ArrayList<Integer>> hashMapGrades = new HashMap<String, ArrayList<Integer>>();
+    List<String> allDataList = new ArrayList<String>();
+    SparkSession spark = SparkSession.builder().appName("Average Conputation").getOrCreate();
 
+    for(String logFile : logFiles){
+      Dataset<String> logData = spark.read().textFile(logFile).cache();
+      allDataList.addAll(logData.collectAsList());
+    }
+
+    for(String data : allDataList){
+      String splitData[] = data.split(" ");
+      // if a student is new, create a new list to storage
+      if(hashMapGrades.get(splitData[0]) == null) {
+        hashMapGrades.put(splitData[0], new ArrayList<Integer>());
+      }
+      hashMapGrades.get(splitData[0]).add(Integer.parseInt(splitData[1]));
+    }
+
+    List<String> resultInList = new ArrayList<String>();
+
+    for(String key : hashMapGrades.keySet()){
+        int sum = 0;
+        for(int grade : hashMapGrades.get(key)){
+            sum += grade;
+        }
+        resultInList.add(String.format("%s,%.2f\n", key, (double)sum / N));
+    }
+    try{
+        BufferedWriter out = new BufferedWriter(new FileWriter("output/result.txt"));
+        for(String str : resultInList){
+          out.write(str);
+        }
+        out.close();
+    }catch(Exception e){}
     spark.stop();
   }
 }
