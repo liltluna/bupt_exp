32df437daec891b5ab5b6db67b99a562d4b91d97
Pick-cmd <728820587@qq.com>
2022-11-25 11:49:56 +0800

    finish the fft long integer computation

--- numerical_analysis/FFT/for.cpp
+++ numerical_analysis/FFT/for.cpp
@@ -6,11 +6,11 @@
 #include <algorithm>
 
 // define this to use M_PI and other parameters
-// to use the "typedef std::vector<unsigned char> Integer"
-// we create a new class Complex instead of using the default one
 #define _USE_MATH_DEFINES
 #include <math.h>
 
+// to use the "typedef std::vector<unsigned char> Integer"
+// we create a new class Complex instead of using the default one
 typedef std::vector<unsigned char> Integer;
 
 class Complex
@@ -129,14 +129,15 @@ void fft(Complex *dist, Complex *src, int N)
     int n = log(N) / log(2);
     rader_reverse(dist, N);
     for (int s = 1, m = 1; s <= n; s++)
-    { //进行迭代过程
+    { 
+        // start the process of iteration
         m *= 2;
         Complex temp, u, omiga, omigaM = Complex(cos(-2 * M_PI / m), sin(-2 * M_PI / m));
         for (int k = 0; k < N; k = k + m)
         {
             omiga = Complex(1);
             for (int j = 0; j <= m / 2 - 1; j++)
-            { //蝶形运算
+            { // butterfly operation
                 temp = omiga * dist[k + j + m / 2];
                 u = dist[k + j];
                 dist[k + j] = u + temp;
@@ -149,27 +150,8 @@ void fft(Complex *dist, Complex *src, int N)
 
 void ifft(Complex *dist, Complex *src, int N)
 {
-    int n = 0;
-    for (int i = 1; i < N; i *= 2)
-    { //求N的二进制位数
-        n++;
-    }
-    for (int i = 0; i <= N - 1; i++)
-    { //进行位反转置换
-        int a = i;
-        int b = 0;
-        for (int j = 0; j < n; j++)
-        { //生成a的反转b
-            b = (b << 1) + (a & 1);
-            a >>= 1;
-        }
-        if (b > i)
-        { //进行置换
-            dist[b] = src[b] + src[i];
-            dist[i] = dist[b] - src[i];
-            dist[b] = dist[b] - dist[i];
-        }
-    }
+    int n = log(N) / log(2);
+    rader_reverse(dist, N);
     for (int s = 1, m = 1; s <= n; s++)
     { // iterative process
         m *= 2;
@@ -195,7 +177,6 @@ void ifft(Complex *dist, Complex *src, int N)
 
 void cmplx_print(Complex &complex)
 {
-    // std::cout<< complex.getReal() << "+" << complex.getImage() << std::endl;
     printf("%lf + %lfi\n", complex.getReal(), complex.getImage());
 }
 
@@ -204,6 +185,20 @@ void int_print(Integer &integer){
         printf("%c", integer.at(i));
     }
     printf("\n");
+}
+
+// this function to transform a vector<unsigned char> to something like vector<int>
+void transform_integer(Integer *integer){
+    for(int i = 0; i < integer->size(); i++){
+        integer->at(i) = integer->at(i) > '9' ? integer->at(i) - '7' : integer->at(i) - '0';
+    }
+}
+
+// this function eliminate the conversion of last function
+void recover_integer(Integer *integer){
+    for(int i = 0; i < integer->size(); i++){
+        integer->at(i) = integer->at(i) > 9 ? integer->at(i) + '7' : integer->at(i) + '0';
+    }
 }
 
 // hand computation
@@ -245,35 +240,103 @@ void hand_computation(Integer *res, Integer *multiplier_l, Integer *multiplier_r
 }
 
 void hand_computation_2(Integer *res, Integer *multiplier_l, Integer *multiplier_r){
-    
-    int left_legth = multiplier_l->size();
+    int b = 16;
+
+    int left_length = multiplier_l->size();
     int right_length = multiplier_r->size();
 
-    for(int i = 0; i < left_legth + right_length; i++){
-        res->at(i) = res -> at(i) - '0';
-    }
+    // transform the vactors
+    transform_integer(res);
+    transform_integer(multiplier_l);
+    if(multiplier_l != multiplier_r)
+        transform_integer(multiplier_r);
 
-    for(int i = 0; i < left_legth; i++){
+    // simulate hand computation
+    for(int i = 0; i < left_length; i++){
         int carry = 0;
         for(int j = 0; j < right_length; j++){
-            int temp = res->at(i + j) + ((multiplier_l->at(i) - '0') * (multiplier_r->at(j) - '0')) + carry;
-            res->at(i + j) = temp % 10;
-            carry = temp / 10;
+            int temp = res->at(i + j) + ((multiplier_l->at(i)) * (multiplier_r->at(j))) + carry;
+            res->at(i + j) = temp % b;
+            carry = temp / b;
         }
         res->at(i + right_length) = carry;
     }
 
-    for(int i = 0; i < left_legth + right_length; i++){
-        res->at(i) = res -> at(i) + '0';
-    }
-            
+    recover_integer(res);
+    recover_integer(multiplier_l);
+    if(multiplier_l != multiplier_r)
+        recover_integer(multiplier_r);
+    
+    // delete the '0's
     int index = res->size() - 1;
-    for(;;index--){
+    for( ; ;index--){
         if(res->at(index) != '0'){
             break;
         }
         res->pop_back();
     }
+
+    std::reverse(res->begin(), res->end());
+}
+
+// realize the long integer multiplication
+void long_integer_with_fft(Integer* res, Integer *integer_left, Integer *integer_right)
+{
+    int base = 16;
+    int left_length, right_length, sum_length;
+
+    left_length = integer_left->size();
+    right_length = integer_right->size();
+    sum_length = left_length + right_length;
+
+    int N = pow(2, (int)(log(sum_length) / log(2)) + 1);
+
+    Complex* left_extend_complex = new Complex[N];
+    Complex* right_extend_complex = new Complex[N];
+    Complex* result_complex = new Complex[N];
+
+    transform_integer(res);
+    transform_integer(integer_right);
+    if(integer_right != integer_left)
+        transform_integer(integer_left);
+
+    for (int i = 0; i < left_length ;i++) {
+        left_extend_complex[i] = Complex((double)(integer_left->at(left_length - i -1)));
+    }
+
+    for (int i = 0; i < right_length ;i++) {
+        right_extend_complex[i] = Complex((double)(integer_right->at(right_length - i -1)));
+    }
+
+    fft(left_extend_complex, left_extend_complex, N);
+    fft(right_extend_complex, right_extend_complex, N);
+
+    for(int i = 0; i < N; i++) {
+        result_complex[i] = left_extend_complex[i] * right_extend_complex[i];
+    }
+
+    ifft(result_complex, result_complex, N);
+
+    int current_number = 0, round_up = 0;
+    for(int i = 0; i < sum_length; i++) {
+        current_number = (int)result_complex[i].getReal() + round_up;
+        res->at(i) = current_number % base;
+        round_up = current_number / base;
+    }
+
+    int index = res->size() - 1;
+    for( ; ;index--){
+        if(res->at(index) != 0){
+            break;
+        }
+        res->pop_back();
+    }
+
+    // recover the integer
+    recover_integer(res);
+    recover_integer(integer_left);
+    if(integer_right != integer_left)
+        recover_integer(integer_left);
 
     std::reverse(res->begin(), res->end());
 }
@@ -308,14 +371,29 @@ void test_fft_ifft()
 
 void test_hand_computation(){
     Integer i_l, res;
-    i_l.resize(40, '1');
+
+    i_l.resize(6, 'A');
     res.resize(100, '0');
+
     hand_computation_2(&res, &i_l, &i_l);
     int_print(i_l);
     int_print(res);
 }
 
-int main()
+void test_long_integer_fft(){
+    Integer i_l, res;
+
+    i_l.resize(6, 'A');
+    res.resize(100, '0');
+
+    long_integer_with_fft(&res, &i_l, &i_l);
+    int_print(i_l);
+    int_print(res);
+}
+
+int main () 
 {
+    test_long_integer_fft();
     test_hand_computation();
+    return 0;
 }
