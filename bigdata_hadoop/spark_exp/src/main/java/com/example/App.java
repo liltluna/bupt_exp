8f21ce3be7b307d9103b9119cc1318944757aad7
Pick-cmd <728820587@qq.com>
2022-11-25 17:22:47 +0800

    config spark

--- bigdata_hadoop/spark_exp/src/main/java/com/example/App.java
+++ bigdata_hadoop/spark_exp/src/main/java/com/example/App.java
@@ -2,17 +2,25 @@ package com.example;
 
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.Dataset;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.SparkConf;
 
-public class SimpleApp {
+public class App {
   public static void main(String[] args) {
-    String logFile = "YOUR_SPARK_HOME/README.md"; // Should be some file on your system
-    SparkSession spark = SparkSession.builder().appName("Simple Application").getOrCreate();
+    SparkConf conf = new SparkConf().setAppName("average calculate").setMaster("local");
+    JavaSparkContext sc = new JavaSparkContext(conf);
+
+    
+    // Should be some file on your system
+    String logFile = "/usr/local/spark/README.md"; 
+    SparkSession spark = SparkSession.builder().appName("Average Calculate").getOrCreate();
     Dataset<String> logData = spark.read().textFile(logFile).cache();
 
-    long numAs = logData.filter(s -> s.contains("a")).count();
-    long numBs = logData.filter(s -> s.contains("b")).count();
+    long numAs = logData.count();
+    // long numBs = logData.filter(s -> s.contains("b")).count();
 
-    System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
+    System.out.println("Lines with a: " + numAs + ", lines with b: " + numAs);
 
     spark.stop();
   }
