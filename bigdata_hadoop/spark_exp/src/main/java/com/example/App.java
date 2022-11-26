605f9ee37a2189c2e95cc59593185d671b39d1f1
Pick-cmd <728820587@qq.com>
2022-11-26 16:13:28 +0800

    format the code

--- bigdata_hadoop/spark_exp/src/main/java/com/example/App.java
+++ bigdata_hadoop/spark_exp/src/main/java/com/example/App.java
@@ -12,7 +12,7 @@ import org.apache.spark.sql.Dataset;
 public class App {
   public static void main(String[] args) {
     // Should be some file on your system
-    String logFiles[] = {"input/db.txt", "input/ds.txt", "input/os.txt"}; 
+    String logFiles[] = { "input/db.txt", "input/ds.txt", "input/os.txt" };
 
     // N records the number of course
     int N = logFiles.length;
@@ -21,15 +21,15 @@ public class App {
     List<String> allDataList = new ArrayList<String>();
     SparkSession spark = SparkSession.builder().appName("Average Conputation").getOrCreate();
 
-    for(String logFile : logFiles){
+    for (String logFile : logFiles) {
       Dataset<String> logData = spark.read().textFile(logFile).cache();
       allDataList.addAll(logData.collectAsList());
     }
 
-    for(String data : allDataList){
+    for (String data : allDataList) {
       String splitData[] = data.split(" ");
       // if a student is new, create a new list to storage
-      if(hashMapGrades.get(splitData[0]) == null) {
+      if (hashMapGrades.get(splitData[0]) == null) {
         hashMapGrades.put(splitData[0], new ArrayList<Integer>());
       }
       hashMapGrades.get(splitData[0]).add(Integer.parseInt(splitData[1]));
@@ -37,20 +37,21 @@ public class App {
 
     List<String> resultInList = new ArrayList<String>();
 
-    for(String key : hashMapGrades.keySet()){
-        int sum = 0;
-        for(int grade : hashMapGrades.get(key)){
-            sum += grade;
-        }
-        resultInList.add(String.format("%s,%.2f\n", key, (double)sum / N));
+    for (String key : hashMapGrades.keySet()) {
+      int sum = 0;
+      for (int grade : hashMapGrades.get(key)) {
+        sum += grade;
+      }
+      resultInList.add(String.format("%s,%.2f\n", key, (double) sum / N));
     }
-    try{
-        BufferedWriter out = new BufferedWriter(new FileWriter("output/result.txt"));
-        for(String str : resultInList){
-          out.write(str);
-        }
-        out.close();
-    }catch(Exception e){}
+    try {
+      BufferedWriter out = new BufferedWriter(new FileWriter("output/result.txt"));
+      for (String str : resultInList) {
+        out.write(str);
+      }
+      out.close();
+    } catch (Exception e) {
+    }
     spark.stop();
   }
 }
